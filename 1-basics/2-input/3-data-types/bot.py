301732e66c4a472c01fe06da26b0a5332332312b
Jack Boswell <3boswj27@solent.ac.uk>
2019-09-26 11:58:13 +0000

    BMI

---
+++ 1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,17 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+
+# Ask user their age
+print("How old are you (in years)?")
+age = int(input())
+
+# Ask user their height
+print("How tall are you (in meters)?")
+height = float(input())
+
+# Ask user their weight
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
+
+print("")
