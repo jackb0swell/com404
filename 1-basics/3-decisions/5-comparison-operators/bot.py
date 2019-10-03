3522bbbecf92989d8ebab9b6c0fc191067440390
Jack Boswell <3boswj27@solent.ac.uk>
2019-10-03 09:59:29 +0000

    changed if to elif

--- 1-basics/3-decisions/5-comparison-operators/bot.py
+++ 1-basics/3-decisions/5-comparison-operators/bot.py
@@ -7,8 +7,8 @@ second_number = int(input())
 if first_number > second_number:
     print("The second number is smallest")
 
-if second_number > first_number:
+elif second_number > first_number:
     print("The first number is smallest")
 
-if first_number == second_number:
+elif first_number == second_number:
     print("Both numbers are the same!")
